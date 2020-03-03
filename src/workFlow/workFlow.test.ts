a6ec6004fc4d356714d9ab8de2675b7ed76046f1
Dennis Hosch <oitio2004@gmail.com>
2020-03-03 08:04:38 +0100

    Add Unittests

--- src/workFlow/workFlow.test.ts
+++ src/workFlow/workFlow.test.ts
@@ -1,6 +1,38 @@
 import workFlow from "./workFlow";
-test('create workFlow', () => {
-   const stateFunctions = {};
-   workFlow({asdf: (goto) => {}}, 'asdf');
-   expect(1).toBe(1)
+test('create workFlow without Errors', () => {
+   const wf = workFlow({
+      a1: (goto) => {
+
+      },
+      a2: (goto) => {
+
+      }}, 'a1');
+   expect(wf().state).toBe('a1')
+});
+
+test('switch States', () => {
+   let changeState = (key:string):object|void => {};
+   const wf = workFlow({
+      a1: (goto) => {
+         changeState = goto;
+      },
+      a2: (goto) => {
+
+      }}, 'a1');
+   changeState('a2');
+   expect(wf().state).toBe('a2')
+});
+
+test('switch States should work only once', () => {
+   let changeState = (key:string):object|void => {};
+   const wf = workFlow({
+      a1: (goto) => {
+         changeState = goto;
+      },
+      a2: (goto) => {
+
+      }}, 'a1');
+   changeState('a2');
+   changeState('a1');
+   expect(wf().state).toBe('a2')
 });
